071243ed07fb57a879c24d02807832b0c4cc93b3
johnrpo-dev <johnsod8729@hotmail.com>
2021-09-20 07:43:57 -0500

    correcion

--- taller2/padawans.js
+++ taller2/padawans.js
@@ -1,17 +1,18 @@
 
 
-
-function asignarPadawan () {
+const matricPadawan = (nombre, edad, planeta, estatura, clasificar) => {
     let padawan = {
-        nombre: 'yuyun',
-        planeta: 'Dead star',
-        edad: 16,
-        estatura: 87
+        nombre: nombre,
+        planeta: planeta,
+        edad: edad,
+        estatura:estatura
     }
-    if(padawan.edad <= 15) {
+    clasificar(padawan.edad)
+}
+matricPadawan( 'Yoda', 14, 'Yavin84', 1.20, edad => {
+    if (edad <=15) {
         console.log('Vas para el manejo de armas');
     }else{
         console.log('Manejaras el sable luz');
-    }   
-}
-console.log(asignarPadawan);
+    }
+})
